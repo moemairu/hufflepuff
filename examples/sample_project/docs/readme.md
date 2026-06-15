45c592f42b175b96d08732ec33c9fec30155c0e8
moemairu <ismailfaruq.qy@gmail.com>
2026-06-16 00:28:51 +0700

    Remove tests and examples directories

--- examples/sample_project/docs/readme.md
+++
@@ -1,2 +0,0 @@
-# Sample Project
-This is a sample project to test the folder archiving capabilities of Hufflepuff.
--- examples/sample_project/src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, sample project!" << std::endl;
-    return 0;
-}
